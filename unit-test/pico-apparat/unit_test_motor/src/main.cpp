48c44261c8046584b51a5a5035155d3c19349997
yarik <iaroslav.muhatdinov@yandex.ru>
2023-03-10 05:09:16 +0300

    fix

--- unit-test/pico-apparat/unit_test_motor/src/main.cpp
+++ unit-test/pico-apparat/unit_test_motor/src/main.cpp
@@ -1,9 +1,24 @@
 #include <Arduino.h>
+#include <ServoSmooth.h>
+
+ServoSmooth servo;
+
+uint32_t tmr;
+boolean flag;
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  servo.attach(5);        // подключить
+  servo.setSpeed(130);    // ограничить скорость
+  servo.setAccel(0.1);   	  // установить ускорение (разгон и торможение)
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  servo.tick();
+
+  if (millis() - tmr >= 3000) {   // каждые 3 сек
+    tmr = millis();
+    flag = !flag;
+    servo.setTargetDeg(flag ? 50 : 120);  // двигаем на углы 50 и 120
+  }
 }
--- unit-test/pico-apparat/unit_test_uart/src/main.cpp
+++ unit-test/pico-apparat/unit_test_uart/src/main.cpp
@@ -1,9 +1,28 @@
 #include <Arduino.h>
+#include <AsyncStream.h>
+#include <GParser.h>
+
+// асинхронная читалка Serial
+// а также любых из класса Stream
+// указываем обработчик, терминатор (символ конца приёма) и таймаут в мс
+// в <> указан размер буфера!
+
+AsyncStream<100> serial(&Serial, '\n');
+
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(57600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // если данные получены
+  if (serial.available()) {     
+    GParser data(serial.buf, ',');
+    int ints[data.amount()];
+    // раскидает в указанный массив и вернёт количество
+    int am2 = data.parseInts(ints);  
+    // выводим
+    Serial.println("");
+    for (byte i = 0; i < am2; i++) Serial.println(ints[i]);
+  }
 }
