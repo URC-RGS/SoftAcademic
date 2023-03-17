1a11192c28eac7acf7eda29b578b3a1c67c9d8fd
yarik <iaroslav.muhatdinov@yandex.ru>
2023-03-17 09:14:23 +0300

    libraly

--- unit-test/pico-apparat/unit_test_servo_cam/src/main.cpp
+++ unit-test/pico-apparat/unit_test_servo_cam/src/main.cpp
@@ -1,9 +1,24 @@
 #include <Arduino.h>
+#include <ServoSmooth.h>
+ServoSmooth servo;
+
+uint32_t tmr;
+boolean flag;
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  servo.attach(3,500, 2400);        // подключить
+  servo.setSpeed(130);    // ограничить скорость
+  servo.setAccel(0.1);   	  // установить ускорение (разгон и торможение)
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  servo.tick();
+
+  if (millis() - tmr >= 3000) {   // каждые 3 сек
+    tmr = millis();
+    flag = !flag;
+    servo.setTargetDeg(flag ? 0 : 180);  // двигаем на углы 50 и 120
+  }
 }
