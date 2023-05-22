9b155178f1080d165edbac7aad2fb9f8fb1fd7b6
yarik <iaroslav.muhatdinov@yandex.ru>
2023-05-22 18:34:00 +0300

    у

--- test/c++/unit_test_uart/src/main.cpp
+++ test/c++/unit_test_uart/src/main.cpp
@@ -7,22 +7,18 @@
 // указываем обработчик, терминатор (символ конца приёма) и таймаут в мс
 // в <> указан размер буфера!
 
-AsyncStream<100> serial(&Serial, '\n');
+AsyncStream<100> serial1(&Serial1, '\n');
 
 
 void setup() {
-  Serial.begin(57600);
+  Serial1.begin(57600);
+  Serial1.setRX(17);
+  Serial1.setTX(16);
 }
 
 void loop() {
   // если данные получены
-  if (serial.available()) {     
-    GParser data(serial.buf, ',');
-    int ints[data.amount()];
-    // раскидает в указанный массив и вернёт количество
-    int am2 = data.parseInts(ints);  
-    // выводим
-    Serial.println("");
-    for (byte i = 0; i < am2; i++) Serial.println(ints[i]);
+  if (serial1.available()) {     
+    Serial.println(serial1.buf);
   }
 }
