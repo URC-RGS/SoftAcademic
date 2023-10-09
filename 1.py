5178c0f30f97b722b4d7b18dd7d9585efe2c2eaa
yarik <iaroslav.muhatdinov@yandex.ru>
2023-10-09 22:14:36 +0300

    refactor_start

---
+++ 1.py
@@ -0,0 +1,9 @@
+
+# добавить при работе с джойстиком 
+from sys import platform
+if platform == "linux" or platform == "linux2":
+    print("linux")
+elif platform == "darwin":
+    print('mac')
+elif platform == "win32":
+    print('windows')
